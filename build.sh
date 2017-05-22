da04ddedeb489f2044eb908b90c8f1b7276cf466
Stephen Hogg <steve@h4.co.nz>
2017-05-22 15:26:48 +1200

    test: add build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+ARTIFACT_DIR="${PWD}/artifacts"
+
+mkdir -p ${ARTIFACT_DIR}
+
+BUILD_NAME="${BUILD_TAG}"
 SERVICES_DIR='services'
-BUILD_COMMAND='npm i && npm run build'
+BUILD_COMMAND='npm i && npm run build && mv .serverless ${ARTIFACT_DIR}/${BUILD_NAME}'
 
 cd "${SERVICES_DIR}/${BUILD_TAG}"
 
