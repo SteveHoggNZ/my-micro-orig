3b54818ae38dfe352098a11a305b3bd272175fe7
Stephen Hogg <steve@h4.co.nz>
2017-05-22 15:18:04 +1200

    test: add build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Build: ${BUILD_TAG}"
+SERVICES_DIR='services'
+BUILD_COMMAND='npm i && npm run build'
+
+cd "${SERVICES_DIR}/${BUILD_TAG}"
+
+echo "=== Running Build ==="
+
+eval ${BUILD_COMMAND}
