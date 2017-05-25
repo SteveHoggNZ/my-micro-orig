51c78388f5aa2c124d69837407b91af685b774a9
Stephen Hogg <steve@h4.co.nz>
2017-05-25 21:00:43 +1200

    fix: create directory before copying

--- scripts/ci-copy-service-source.sh
+++ scripts/ci-copy-service-source.sh
@@ -12,7 +12,7 @@ buildSplit=(${BUILD_TAG//\// })
 SERVICE=${buildSplit[0]}
 VERSION=${buildSplit[1]}
 
-COPY_COMMANDS='cp -R . ${S3_SRC_DIR}/${SERVICE}/${VERSION}'
+COPY_COMMANDS='mkdir -p ${S3_SRC_DIR}/${SERVICE}/ && cp -R . ${S3_SRC_DIR}/${SERVICE}/${VERSION}'
 
 cd "${SERVICES_DIR}/${SERVICE}"
 
