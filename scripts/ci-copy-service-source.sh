01630a6dfbe484022d113c0a3cd91a1c9fa59822
Stephen Hogg <steve@h4.co.nz>
2017-05-25 20:31:30 +1200

    add copy scripts/ci-copy-service-source.sh script

---
+++ scripts/ci-copy-service-source.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+S3_SRC_DIR="${PWD}/src"
+
+mkdir -p ${S3_SRC_DIR}
+
+SERVICES_DIR='services'
+
+buildSplit=(${BUILD_TAG//\// })
+SERVICE=${buildSplit[0]}
+VERSION=${buildSplit[1]}
+
+COPY_COMMANDS='cp . ${S3_SRC_DIR}/${SERVICE}/${VERSION}'
+
+cd "${SERVICES_DIR}/${SERVICE}"
+
+echo "=== Running Copy ==="
+
+eval "${COPY_COMMANDS}"
