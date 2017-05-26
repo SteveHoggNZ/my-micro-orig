633310c688b3c9b75568efdee95a63758fdaccc7
Stephen Hogg <steve@h4.co.nz>
2017-05-26 15:23:26 +1200

    fix: debug ci-archive-to-s3.sh script

--- scripts/ci-archive-to-s3.sh
+++ scripts/ci-archive-to-s3.sh
@@ -14,7 +14,7 @@ VERSION=${buildSplit[1]}
 
 cd "${SERVICES_DIR}/${SERVICE}"
 
-COPY_COMMANDS='mkdir -p ${S3_QUEUE_DIR}/${SERVICE}/ && zip -r ${S3_QUEUE_DIR}/${SERVICE}.zip .'
+COPY_COMMANDS='pwd && ls -al && mkdir -p ${S3_QUEUE_DIR}/${SERVICE}/ && zip -r ${S3_QUEUE_DIR}/${SERVICE}.zip .'
 
 echo "=== Running Copy ==="
 
