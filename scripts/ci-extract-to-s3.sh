7cc4216b6d5f9aec970c532a65b11bb6a231edc4
Stephen Hogg <steve@h4.co.nz>
2017-05-27 13:59:36 +1200

    feat: add check for package name and version

--- scripts/ci-extract-to-s3.sh
+++ scripts/ci-extract-to-s3.sh
@@ -14,6 +14,19 @@ VERSION=${buildSplit[1]}
 
 cd "${SERVICES_DIR}/${SERVICE}"
 
+CHECK_SERVICE=$(node -p "require('./package.json').name")
+CHECK_VERSION=$(node -p "require('./package.json').version")
+
+if [[ "${SERVICE}" != "${CHECK_SERVICE}" ]]; then
+  echo "Package service ${CHECK_SERVICE} does not match tag service ${SERVICE}"
+  exit 2
+fi
+
+if [[ "${VERSION}" != "${CHECK_VERSION}" ]]; then
+  echo "Package version ${CHECK_VERSION} does not match tag version ${VERSION}"
+  exit 2
+fi
+
 EXTRACT_COMMANDS='cp ../../.eslintrc . && mkdir -p ${S3_QUEUE_DIR}/${SERVICE}/ && zip -r ${S3_QUEUE_DIR}/${SERVICE}.zip .'
 
 echo "=== Running Extract ==="
