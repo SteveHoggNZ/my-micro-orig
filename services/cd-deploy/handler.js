ae077f2c46f07ab09b73f7d455eaf740eb3c2090
Stephen Hogg <steve@h4.co.nz>
2017-05-28 15:45:29 +1200

    fix: debug buildspec.yml

--- services/cd-deploy/handler.js
+++ services/cd-deploy/handler.js
@@ -2,36 +2,48 @@
 
 const AWS = require('aws-sdk')
 
-module.exports.deploy = (event, context, callback) => {
-  const codepipeline = new AWS.CodePipeline()
+// module.exports.deploy = (event, context, callback) => {
+//   const codepipeline = new AWS.CodePipeline()
+//
+//   // Retrieve the Job ID from the Lambda action
+//   const jobId = event["CodePipeline.job"].id
+//
+//   // Notify AWS CodePipeline of a successful job
+//   const putJobSuccess = function(message) {
+//       const params = {
+//           jobId: jobId
+//       }
+//
+//       codepipeline.putJobSuccessResult(params, function(err, data) {
+//           if(err) {
+//             callback(err)
+//           } else {
+//             const response = {
+//               statusCode: 200,
+//               body: JSON.stringify({
+//                 message,
+//                 input: event
+//               })
+//             }
+//
+//             callback(null, response)
+//           }
+//       })
+//   }
+//
+//   console.log('Code Pipeline Event:', JSON.stringify(event))
+//
+//   putJobSuccess('I can deploy!')
+// }
 
-  // Retrieve the Job ID from the Lambda action
-  const jobId = event["CodePipeline.job"].id
-
-  // Notify AWS CodePipeline of a successful job
-  const putJobSuccess = function(message) {
-      const params = {
-          jobId: jobId
-      }
-
-      codepipeline.putJobSuccessResult(params, function(err, data) {
-          if(err) {
-            callback(err)
-          } else {
-            const response = {
-              statusCode: 200,
-              body: JSON.stringify({
-                message,
-                input: event
-              })
-            }
-
-            callback(null, response)
-          }
-      })
+module.exports.sup = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'sup2',
+      input: event
+    })
   }
 
-  console.log('Code Pipeline Event:', JSON.stringify(event))
-
-  putJobSuccess('I can deploy!')
+  callback(null, response)
 }
