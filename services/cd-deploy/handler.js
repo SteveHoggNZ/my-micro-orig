f31ada52803d0065e4f74186cc2fd2463d2035c6
Stephen Hogg <steve@h4.co.nz>
2017-05-28 16:39:14 +1200

    fix: debug buildspec.yml

--- services/cd-deploy/handler.js
+++ services/cd-deploy/handler.js
@@ -2,45 +2,11 @@
 
 const AWS = require('aws-sdk')
 
-// module.exports.deploy = (event, context, callback) => {
-//   const codepipeline = new AWS.CodePipeline()
-//
-//   // Retrieve the Job ID from the Lambda action
-//   const jobId = event["CodePipeline.job"].id
-//
-//   // Notify AWS CodePipeline of a successful job
-//   const putJobSuccess = function(message) {
-//       const params = {
-//           jobId: jobId
-//       }
-//
-//       codepipeline.putJobSuccessResult(params, function(err, data) {
-//           if(err) {
-//             callback(err)
-//           } else {
-//             const response = {
-//               statusCode: 200,
-//               body: JSON.stringify({
-//                 message,
-//                 input: event
-//               })
-//             }
-//
-//             callback(null, response)
-//           }
-//       })
-//   }
-//
-//   console.log('Code Pipeline Event:', JSON.stringify(event))
-//
-//   putJobSuccess('I can deploy!')
-// }
-
 module.exports.sup = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Sup, 3rd time a charm!!!',
+      message: 'Sup, 4th time a charm!!!',
       input: event
     })
   }
