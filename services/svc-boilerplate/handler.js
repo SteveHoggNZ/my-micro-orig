37dc74ab7691fdab357369bd0d1e2096c3917613
Stephen Hogg <steve@h4.co.nz>
2017-05-28 20:20:59 +1200

    test: boil release

--- services/svc-boilerplate/handler.js
+++ services/svc-boilerplate/handler.js
@@ -3,17 +3,13 @@
 const AWS = require('aws-sdk')
 
 module.exports.boil = (event, context, callback) => {
-  console.log(process.env)
-  const version = process.env.SVC_VERSION
-
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Successfully boiled version ${version}`,
+      message: `Successfully boiled version ${process.env.SVC_VERSION}`,
       input: event
     })
   }
 
   callback(null, response)
 }
-SVC_VERSION
