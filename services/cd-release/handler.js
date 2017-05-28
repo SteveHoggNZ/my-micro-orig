d312d32e3729cd4e581c723081c5079df3983123
Stephen Hogg <steve@h4.co.nz>
2017-05-29 09:07:58 +1200

    test: boil release

---
+++ services/cd-release/handler.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+
+module.exports.pre_release = (event, context, callback) => {
+  const codepipeline = new AWS.CodePipeline()
+
+  // Retrieve the Job ID from the Lambda action
+  const jobId = event["CodePipeline.job"].id
+
+  // Notify AWS CodePipeline of a successful job
+  const putJobSuccess = function(message) {
+      const params = {
+          jobId: jobId
+      }
+
+      codepipeline.putJobSuccessResult(params, function(err, data) {
+          if(err) {
+            callback(err)
+          } else {
+            const response = {
+              statusCode: 200,
+              body: JSON.stringify({
+                message,
+                input: event
+              })
+            }
+
+            callback(null, response)
+          }
+      })
+  }
+
+  console.log('Code Pipeline Event:', JSON.stringify(event))
+
+  putJobSuccess(`Successfully pre-released (pre-release version) ${process.env.SVC_VERSION}`)
+}
+
+module.exports.release = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Successfully released version ${process.env.SVC_VERSION}`,
+      input: event
+    })
+  }
+
+  callback(null, response)
+}
