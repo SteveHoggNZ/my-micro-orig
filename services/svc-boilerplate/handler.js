6789b7e82a83c11766a689e054480a98d6775ddc
Stephen Hogg <steve@h4.co.nz>
2017-05-28 19:51:04 +1200

    feat: initial commit of boilerplate service

--- services/svc-boilerplate/handler.js
+++ services/svc-boilerplate/handler.js
@@ -2,11 +2,11 @@
 
 const AWS = require('aws-sdk')
 
-module.exports.sup = (event, context, callback) => {
+module.exports.boil = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Sup, 5th time a charm!!!',
+      message: 'Successfully boiled!!!',
       input: event
     })
   }
